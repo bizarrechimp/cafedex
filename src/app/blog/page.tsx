3e8acb7635e71a742008128fa0e7d481ff731cbd
Alex Gonzalez <alexgr946@gmail.com>
2025-04-11 22:40:32 +0200

    fix: Make key prop usage more explicit in blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,22 +9,25 @@ export default async function BlogPage() {
       <h1 className="text-3xl font-bold mb-8">Blog</h1>
 
       <div className="flex flex-wrap justify-center gap-8">
-        {posts.map((post) => (
-          <div key={post.slug} className="w-full lg:w-1/3 md:w-1/2 px-4">
-            <BlogCard
-              title={post.title}
-              excerpt={post.excerpt}
-              image={post.coverImage}
-              date={new Date(post.date).toLocaleDateString('es-ES', {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric'
-              })}
-              slug={post.slug}
-              author={post.author}
-            />
-          </div>
-        ))}
+        {posts.map((post) => {
+          const key = post.slug;
+          return (
+            <div key={key} className="w-full lg:w-1/3 md:w-1/2 px-4">
+              <BlogCard
+                title={post.title}
+                excerpt={post.excerpt}
+                image={post.coverImage}
+                date={new Date(post.date).toLocaleDateString('es-ES', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric'
+                })}
+                slug={post.slug}
+                author={post.author}
+              />
+            </div>
+          );
+        })}
       </div>
     </main>
   );
